5cbc6f29ba7489e1738d64abe2b57f1a34457454
julia <julia@DESKTOP-4ELTGFP>
2024-08-28 14:40:15 -0300

    Add project files.

---
+++ CleanArch.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using CleanArch.Domain.Entities;
+
+namespace CleanArch.Domain.Interfaces;
+    public interface IProductRepository
+    {
+        Task <IEnumerable<Product>> GetProducts();
+        
+        Task<Product> GetById(int? id);
+        
+        void Add(Product product);
+        
+        void Update(Product product);
+        
+        void Remove(Product product);
+
+    }
---
+++ CleanArch.MVC/Program.cs
@@ -0,0 +1,57 @@
+using CleanArch.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using CleanArch.Infra.IoC;
+using CleanArch.MVC.MappingConfig;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Services configurtations
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddAutoMapperConfiguration();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ocorreu um erro na migração/alimentação dos dados");
+    }
+}
+
+app.Run();
